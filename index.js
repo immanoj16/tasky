586d30374dd3c00735609b355a6896bc2af9b956
immanoj16 <mmaharana14@gmail.com>
2018-02-03 20:45:39 +0530

    Add: custom timer tray class

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, Tray } = require('electron');
 const path = require('path');
 const url = require('url');
+const TimerTray = require('./app/timer_tray');
 
 let mainWindow;
 let tray;
@@ -23,7 +24,7 @@ app.on('ready', () => {
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
-  tray = new Tray(iconPath);
+  tray = new TimerTray(iconPath);
 
   tray.on('click', (event, bounds) => {
 
