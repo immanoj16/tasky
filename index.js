602ebe63f23d7dea31c1c4fa339625e4c0cc5ddb
immanoj16 <mmaharana14@gmail.com>
2018-02-03 18:55:59 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow, Tray } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 300,
+    height: 500,
+    frame: false,
+    resizable: false
+  });
+
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, './src/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
+  const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
+
+  new Tray(iconPath);
+});
