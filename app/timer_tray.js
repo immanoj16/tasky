e63083f595b6792d80758ff4c6d887c14e45847c
immanoj16 <mmaharana14@gmail.com>
2018-02-06 09:12:06 +0530

    Add: too tip to the icon to show the name

--- app/timer_tray.js
+++ app/timer_tray.js
@@ -1,8 +1,34 @@
 const { Tray } = require('electron');
 
 class TimerTray extends Tray {
-  constructor(iconPath) {
-    super(iconPath)
+  constructor(iconPath, mainWindow) {
+    super(iconPath);
+
+    this.mainWindow = mainWindow;
+
+    this.setToolTip('Timer App');
+    this.on('click', this.onClick.bind(this));
+  }
+
+  onClick (event, bounds) {
+    const { x, y } = bounds;
+
+    const { height, width } = this.mainWindow.getBounds();
+
+    if (this.mainWindow.isVisible()) {
+      this.mainWindow.hide()
+    } else {
+
+      const yPosition = process.platform === 'win32' ? y - height : y;
+
+      this.mainWindow.setBounds({
+        x: 980,  // x - width / 2
+        y: yPosition,
+        height,
+        width
+      });
+      this.mainWindow.show()
+    }
   }
 }
 
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const url = require('url');
 const TimerTray = require('./app/timer_tray');
 
 let mainWindow;
-let tray;
 
 app.on('ready', () => {
   mainWindow = new BrowserWindow({
@@ -24,37 +23,5 @@ app.on('ready', () => {
   const iconName = process.platform === 'win32' ? 'windows-icon.png' : 'iconTemplate.png';
   const iconPath = path.join(__dirname, `./src/assets/${iconName}`);
 
-  tray = new TimerTray(iconPath);
-
-  tray.on('click', (event, bounds) => {
-
-    console.log(bounds.x, bounds.y);
-
-    const { x, y } = bounds;
-
-    const { height, width } = mainWindow.getBounds();
-
-    if (mainWindow.isVisible()) {
-      mainWindow.hide()
-    } else {
-
-      const yPosition = process.platform === 'win32' ? y - height : y;
-
-      mainWindow.setBounds({
-        x: 980,  // x - width / 2
-        y: yPosition,
-        height,
-        width
-      });
-      mainWindow.show()
-    }
-  });
-
-  mainWindow.on('show', () => {
-    tray.setHighlightMode('always')
-  });
-
-  mainWindow.on('hide', () => {
-    tray.setHighlightMode('never')
-  });
+  new TimerTray(iconPath, mainWindow);
 });
